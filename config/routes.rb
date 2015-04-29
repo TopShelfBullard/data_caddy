24322e1e6068ee323068b0d035f7e8b6f4f3768f
TopShelfBullard <amb@amitchellbullard.com>
2015-04-29 00:16:54 -0500

    Set up player model with devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :rounds do
+    resources :course
+  end
+
+  devise_for :players
+
   root 'rounds#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ db/schema.rb
@@ -0,0 +1,81 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150429044204) do
+
+  create_table "clubs", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "holes", force: :cascade do |t|
+    t.integer  "number"
+    t.integer  "par"
+    t.integer  "distance"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string   "name",                   default: "", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "players", ["email"], name: "index_players_on_email", unique: true
+  add_index "players", ["name"], name: "index_players_on_name", unique: true
+  add_index "players", ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
+
+  create_table "rounds", force: :cascade do |t|
+    t.integer  "course_id"
+    t.integer  "player_id"
+    t.date     "date"
+    t.integer  "score"
+    t.boolean  "finished"
+    t.integer  "opponent_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "shots", force: :cascade do |t|
+    t.integer  "round_id"
+    t.integer  "hole_id"
+    t.integer  "club_id"
+    t.integer  "number"
+    t.integer  "mulligan"
+    t.boolean  "slice"
+    t.boolean  "fade"
+    t.boolean  "squib"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
