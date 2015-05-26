a54188e1a1156fac5c514025268b8b3ab3c39612
TopShelfBullard <amb@amitchellbullard.com>
2015-05-26 00:42:47 -0500

    Add columns to club.

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -2,6 +2,6 @@ class ClubsController < ApplicationController
 
   private
   def round_params
-    params.require(:club).permit(:name)
+    params.require(:club).permit(:name, :head, :shaft, :abbreviation)
   end
 end
--- db/migrate/20150429042426_create_clubs.rb
+++ db/migrate/20150429042426_create_clubs.rb
@@ -2,6 +2,9 @@ class CreateClubs < ActiveRecord::Migration
   def change
     create_table :clubs do |t|
       t.string :name
+      t.string :head
+      t.string :shaft
+      t.string :abbreviation
       t.timestamps null: false
     end
   end
