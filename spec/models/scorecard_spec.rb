ad695a2bad503d79928a9390bf3aba4b100bef72
TopShelfBullard <amb@amitchellbullard.com>
2015-06-02 18:57:58 -0500

    Get basic testing around the scorecard.

--- spec/models/scorecard_spec.rb
+++ spec/models/scorecard_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe Scorecard, type: :model do
     data = Scorecard.set_scorecard_hash(round)
     expect(data.first[:mulligans]).to eq(2)
   end
+
+  it "properly sets the number of drops taken during a hole" do
+    round = Round.find(1)
+    data = Scorecard.set_scorecard_hash(round)
+    expect(data.first[:drops]).to eq(2)
+  end
+
+  it "properly sets the number of practice shots taken during a hole" do
+    round = Round.find(1)
+    data = Scorecard.set_scorecard_hash(round)
+    expect(data.first[:practice_shots]).to eq(2)
+  end
+
+  it "properly sets the number of putts taken during a hole" do
+    round = Round.find(1)
+    data = Scorecard.set_scorecard_hash(round)
+    expect(data.first[:putts]).to eq(3)
+  end
+
+  it "properly sets the score for a hole" do
+    round = Round.find(1)
+    data = Scorecard.set_scorecard_hash(round)
+    expect(data.first[:score]).to eq(7)
+  end
 end
