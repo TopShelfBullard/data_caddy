e600a4440863cba6b9574b116b754882d9b85ae2
TopShelfBullard <amb@amitchellbullard.com>
2015-06-14 22:36:10 -0500

    Separate shot result entry from ly entry.

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -27,11 +27,12 @@ class ShotsController < ApplicationController
 
   def update
     @shot.update(shot_params)
-
     if @shot.cup
       redirect_to round_path(@played_hole.round)
+    elsif !@shot.has_prepared || !@shot.has_evaluated || !@shot.entered_ly
+      redirect_to edit_played_hole_shot_path(@played_hole, @shot)
     else
-      redirect_to played_hole_path(@played_hole)
+      redirect_to @played_hole
     end
   end
 
@@ -50,11 +51,15 @@ class ShotsController < ApplicationController
   end
 
   def set_shot_status
-    @is_tee = @shot.is_off_the_tee
     @prep_entered = @shot.has_prepared
     @result_entered = @shot.has_evaluated
+    @ly_entered = @shot.entered_ly
+
+    @all_entered = @prep_entered && @result_entered && @ly_entered
+
+    @is_tee = @shot.is_off_the_tee
     @is_putting = @shot.is_a_putt
-    @all_entered = @prep_entered && @result_entered
+
     @club_options = Club.all.map{|club| ["#{club.name}", club.id]}
 
     @shot.green = @is_putting
